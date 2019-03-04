8f96d15468bdea0154f73364391d2659dd1f8d74
MBOYIRE <mboyire14@gmail.com>
2019-03-04 16:49:25 +0200

    adding adding production in manage.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     QUOTE_API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:cycy1234@localhost/blloogz'
+    
 #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
