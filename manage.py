eeac7489dede2fd2964f23d19dfce4a771881524
KAYITARES <45564105+KAYITARES@users.noreply.github.com>
2019-03-06 09:09:13 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -8,7 +8,14 @@ from  flask_migrate import Migrate, MigrateCommand
 app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
-
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
